c192138b986985a7c8bf895e11820564a6ecd159
Karan Shetty <shettykaran2110@gmail.com>
2021-05-30 14:19:02 +0530

    Firebase setup

---
+++ src/adapters/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+export const authentication = firebase
+  .initializeApp({
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: 'chatlance-62de9.firebaseapp.com',
+    projectId: 'chatlance-62de9',
+    storageBucket: 'chatlance-62de9.appspot.com',
+    messagingSenderId: '265058624131',
+    appId: '1:265058624131:web:e8ce3bf7adc75d95cef1b6',
+  })
+  .auth();
