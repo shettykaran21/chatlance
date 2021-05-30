7f5dc15e67640abda5ef52b258183bd0aa4facde
Karan Shetty <shettykaran2110@gmail.com>
2021-05-30 13:50:15 +0530

    Installed required dependencies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return <div className="App">App</div>;
-}
+import Login from './pages/Login';
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+};
 
 export default App;
