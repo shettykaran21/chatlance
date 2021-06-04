a9a9781a03067daf1b2f81a6424c9156091def3c
Karan Shetty <shettykaran2110@gmail.com>
2021-06-04 20:13:43 +0530

    Chats page done

--- src/pages/Chats.js
+++ src/pages/Chats.js
@@ -1,20 +1,72 @@
 import { ChatEngine } from 'react-chat-engine';
+import axios from 'axios';
 
 import Navbar from 'components/Layout/Navbar';
 import useAuth from 'hooks/useAuth';
+import { useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
 
 const Chats = () => {
   const { user } = useAuth();
-  console.log(user);
+  const [isLoading, setIsLoading] = useState(true);
+  const history = useHistory();
+
+  const getImageFile = async (url) => {
+    const response = await fetch(url);
+    const data = await response.blob();
+
+    return new File([data], 'user.jpg', { type: 'image/jpeg' });
+  };
+
+  const getUserData = async () => {
+    try {
+      await axios.get('https://api.chatengine.io/users/me', {
+        headers: {
+          'project-id': process.env.REACT_APP_CHAT_ENGINE_PROJECT_ID,
+          'user-name': user.email,
+          'user-secret': user.uid,
+        },
+      });
+      setIsLoading(false);
+    } catch (err) {
+      let formData = new FormData();
+      formData.append('email', user.email);
+      formData.append('username', user.email);
+      formData.append('secret', user.uid);
+
+      getImageFile(user.photoURL).then((avatar) => {
+        formData.append('avatar', avatar, avatar.name);
+
+        axios
+          .post('https://api.chatengine.io/users/', formData, {
+            headers: {
+              'private-key': process.env.REACT_APP_CHAT_ENGINE_KEY,
+            },
+          })
+          .then(() => setIsLoading(false))
+          .catch((err) => console.log(err));
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (!user) {
+      history.push('/');
+      return;
+    }
+
+    getUserData();
+  });
 
   return (
     <div>
       <Navbar />
+      {isLoading || (!user && <div>Loading...</div>)}
       <ChatEngine
         height="calc(100vh - 66px)"
-        projectId={process.env.REACT_APP_CHAT_ENGINE_PROJECT_ID}
-        userName="."
-        userSecret="."
+        projectID={process.env.REACT_APP_CHAT_ENGINE_PROJECT_ID}
+        userName={user?.email}
+        userSecret={user?.uid}
       />
     </div>
   );
